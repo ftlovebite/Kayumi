f42d86dd75d73282e2fbab3b8b65346f26fb8037
Light <177438854+ftlovebite@users.noreply.github.com>
2026-07-18 21:58:45 +0530

    Implement custom logging formatter in logger.py

---
+++ core/logger.py
@@ -0,0 +1,25 @@
+from core.logger import logger
+import sys
+
+class CustomFormatter(logging.Formatter):
+    FORMATS = {
+        logging.DEBUG: "[DEBUG] %(message)s",
+        logging.INFO: "[INFO] %(message)s",
+        logging.WARNING: "[WARNING] %(message)s",
+        logging.ERROR: "[ERROR] %(message)s",
+        logging.CRITICAL: "[CRITICAL] %(message)s",
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+logger = logging.getLogger("Kayumi")
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(CustomFormatter())
+
+logger.addHandler(handler)
