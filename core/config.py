19ab56561e0823a634c2f7734f845d1d39b9614f
Light <177438854+ftlovebite@users.noreply.github.com>
2026-07-18 20:28:53 +0530

    Add UTF-8 encoding to config file read

--- core/config.py
+++ core/config.py
@@ -1,4 +1,8 @@
 import json
 
-with open("config.json", "r") as f:
+with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
+
+
+def is_owner(user_id: int):
+    return user_id in config.get("owners", [])
