53c1fed84805d26c4a7d36c69e1f1db825a77a55
Light <177438854+ftlovebite@users.noreply.github.com>
2026-07-20 09:23:43 +0530

    Update player.py

--- music/player.py
+++ music/player.py
@@ -4,9 +4,18 @@ from music.queue import MusicQueue
 class GuildPlayer:
     def __init__(self):
         self.queue = MusicQueue()
+
         self.voice_client = None
+
         self.current = None
-        self.volume = 80
+
+        self.volume = 0.8
+
+        self.is_playing = False
+
+        self.text_channel = None
+
+        self.now_playing_message = None
 
 
 players = {}
