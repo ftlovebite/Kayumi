5a79370a100b7870fd2b7d3933e705d4774e038f
Light <177438854+ftlovebite@users.noreply.github.com>
2026-07-23 15:14:17 +0530

    Update song.py

--- music/song.py
+++ music/song.py
@@ -1,12 +1,1 @@
-from dataclasses import dataclass
 
-
-@dataclass
-class Song:
-    title: str
-    webpage_url: str
-    stream_url: str
-    duration: int
-    thumbnail: str
-    uploader: str
-    requester: int
